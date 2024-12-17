8cd7a0b8d4d9da094c51f83d01ec47337234ef1a
JonasDeBruyne <jonasdebruyne734@gmail.com>
2024-12-17 23:16:16 +0100

    feat: partnerschema updated

--- models/api/v1/Partner.js
+++ models/api/v1/Partner.js
@@ -1,56 +1,19 @@
 const mongoose = require("mongoose");
 
 const partnerSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  address: {
-    type: Object,
-    default: {},
-  },
-  contact_email: {
-    type: String,
-    // Maak contact_email optioneel en accepteer null of een leeg veld
-    required: false,
-    validate: {
-      validator: function (v) {
-        // E-mail validatie enkel als er een waarde is (laat null of leeg toe)
-        return !v || /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(v);
-      },
-      message: "Ongeldig e-mailadres!",
-    },
-  },
-  contact_phone: {
-    type: String,
-    required: false,
-  },
-  package: {
-    type: String,
-    enum: ["standard", "pro"],
-    default: "standard",
-    required: true,
-  },
-  primary_color: {
-    type: String,
-    required: true,
-    match: /^#([0-9A-F]{3}){1,2}$/i,
-  },
-  secondary_color: {
-    type: String,
-    required: true,
-    match: /^#([0-9A-F]{3}){1,2}$/i,
-  },
-  titles_color: { type: String, required: true },
-  text_color: { type: String, required: true, match: /^#([0-9A-F]{3}){1,2}$/i },
-  background_color: {
-    type: String,
-    required: true,
-    match: /^#([0-9A-F]{3}){1,2}$/i,
-  },
+  name: { type: String, required: true },
+  address: { type: Object, default: {} },
+  contact_email: { type: String, default: null },
+  contact_phone: { type: String, default: null },
+  package: { type: String, required: true },
+  primary_color: { type: String, required: true }, // RGB string
+  secondary_color: { type: String, required: true }, // RGB string
+  titles_color: { type: String, required: true }, // RGB string
+  text_color: { type: String, required: true }, // RGB string
+  background_color: { type: String, required: true }, // RGB string
   fontFamilyBodyText: { type: String, required: true },
   fontFamilyTitles: { type: String, required: true },
-  logo_url: { type: String, required: false }, // Added logo_url field
+  logo_url: { type: String, default: null },
 });
 
 const Partner = mongoose.model("Partner", partnerSchema);
