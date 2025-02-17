b48793acccf12fb3533f835cd3cbe829630b4ddb
JonasDeBruyne <jonasdebruyne734@gmail.com>
2025-02-17 13:19:15 +0100

    fix: issue app.js

--- app.js
+++ app.js
@@ -92,27 +92,6 @@ app.use(async (req, res, next) => {
   const host = req.headers.host; // bv. "odettelunettes.rebilt.be"
   const subdomain = host.split(".")[0]; // "odettelunettes"
   console.log("Subdomein herkend:", subdomain); // Dit geeft aan welk subdomein wordt gedetecteerd
-
-  // Controleer of het een geldig subdomein is
-  if (subdomain !== "www") {
-    const Partner = require("./models/api/v1/Partner");
-
-    try {
-      const partner = await Partner.findOne({ subdomain });
-      if (!partner) {
-        console.log("Partner niet gevonden voor subdomein:", subdomain);
-        return res.status(404).send("Subdomein niet gevonden");
-      }
-
-      req.partner = partner; // Opslaan van partnergegevens voor later gebruik
-      console.log("Partnergegevens geladen:", partner); // Toon de partnergegevens
-    } catch (error) {
-      console.error("Fout bij ophalen partner:", error);
-      return res.status(500).send("Interne serverfout");
-    }
-  }
-
-  next();
 });
 
 app.use(subdomainRouter);
