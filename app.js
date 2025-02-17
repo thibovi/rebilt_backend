4ff5c1e6e9877c3cc32930d479531f7fb487f07e
JonasDeBruyne <jonasdebruyne734@gmail.com>
2025-02-17 17:42:40 +0100

    fix: distfolder

--- app.js
+++ app.js
@@ -65,7 +65,7 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "dist")));
 
 // Subdomein middleware voor partner ophalen
 app.use(async (req, res, next) => {
