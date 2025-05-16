a624271b91bd17c39201658cbb3884f5622d9094
JonasDeBruyne <jonasdebruyne734@gmail.com>
2025-05-16 22:30:44 +0200

    fix: update

--- controllers/api/v1/partnerConfiguration.js
+++ controllers/api/v1/partnerConfiguration.js
@@ -97,35 +97,46 @@ const show = async (req, res) => {
   }
 };
 
+// Update Partner Configuration
 const update = async (req, res) => {
   try {
-    const updateFields = {};
-    // Voeg alleen velden toe die in req.body zitten
-    Object.keys(req.body).forEach((key) => {
-      updateFields[key] = req.body[key];
-    });
+    const { partnerId, configurationId, options, categoryIds } = req.body;
 
-    const updatedPartner = await Partner.findByIdAndUpdate(
+    if (options && !Array.isArray(options)) {
+      return res.status(400).json({
+        status: "error",
+        message: "Options must be an array",
+      });
+    }
+
+    // Alleen velden updaten die meegestuurd zijn
+    const updateFields = {};
+    if (partnerId !== undefined) updateFields.partnerId = partnerId;
+    if (configurationId !== undefined)
+      updateFields.configurationId = configurationId;
+    if (categoryIds !== undefined) updateFields.categoryIds = categoryIds;
+    if (options !== undefined) updateFields.options = options;
+
+    const updatedPartnerConfig = await PartnerConfiguration.findByIdAndUpdate(
       req.params.id,
       updateFields,
       { new: true }
     );
 
-    if (!updatedPartner) {
-      return res.status(404).json({
-        status: "error",
-        message: "Partner not found",
-      });
+    if (!updatedPartnerConfig) {
+      return res
+        .status(404)
+        .json({ status: "error", message: "Partner Configuration not found" });
     }
 
     res.status(200).json({
       status: "success",
-      data: updatedPartner,
+      data: updatedPartnerConfig,
     });
   } catch (error) {
     res.status(500).json({
       status: "error",
-      message: "Error updating partner",
+      message: "Error updating partner configuration",
       error: error.message,
     });
   }
