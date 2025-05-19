a93299310d83d0b45efdd724974b4474eff6ac2d
JonasDeBruyne <jonasdebruyne734@gmail.com>
2025-05-20 01:56:48 +0200

    fix: routes

--- routes/api/v1/users.js
+++ routes/api/v1/users.js
@@ -4,9 +4,9 @@ const authController = require("../../../controllers/api/v1/auth");
 
 router.post("/signup", authController.signup);
 router.post("/login", authController.login);
+router.get("/check-email", authController.checkEmailExists);
 router.get("/", authController.index);
 router.get("/:id", authController.show);
-router.get("/check-email", authController.checkEmailExists);
 router.put("/:id", authController.update);
 router.delete("/:id", authController.destroy);
 router.post("/forgot-password", authController.forgotPassword);
